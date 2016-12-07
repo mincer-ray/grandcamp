56f352dbf9eb66de90a629e5e83088536b863c2e
Peter De Genaro <pdegenaro@gmail.com>
2016-12-07 11:35:40 -0500

    repair root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
+import ArtistReducer from './artist_reducer';
 
 const rootReducer = combineReducers({
   session: SessionReducer,
